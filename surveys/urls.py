d21b961dfc7fbe600b58d92943a60e2802f17290
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2026-01-13 22:52:25 -0300

    feat(surveys): add urls

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import survey_response_create
+from .views import survey_response_create, survey_already_answered, survey_success
+
+app_name = "surveys"
 
 urlpatterns = [
-    path("responder/", survey_response_create, name="survey_response"),
+    path("responder/", survey_response_create, name="responder"),
+    path("ja-respondeu/", survey_already_answered, name="already_answered"),
+    path("sucesso/", survey_success, name="success"),
 ]
