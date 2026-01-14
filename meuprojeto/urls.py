93cfffcedaa90af84667c7dc95e295a40d2da54d
BeiruthDEV <matheusbeiruth10@gmail.com>
2026-01-14 20:07:03 -0300

    update

--- meuprojeto/urls.py
+++ meuprojeto/urls.py
@@ -1,55 +1,47 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin_profile'),
-
-        #incluindo as URLs do app accounts
+    path("", RedirectView.as_view(url="/accounts/login/", permanent=False)),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("companies/", include("companies.urls")),
+    path("projects/", include("projects.urls")),
+    path("surveys/", include("surveys.urls")),
     path(
-        'accounts/',
-        include('accounts.urls')
+        "accounts/password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html"
         ),
-
+        name="password_reset",
+    ),
     path(
-        'login/',
-         RedirectView.as_view
-         (url='/accounts/login/',
-        permanent=True)
+        "accounts/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html"
         ),
-
-    # URLs de reset de senha padrão do Django
-    path('accounts/password_reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset_form.html'
-         ),
-         name='password_reset'),
-
-    path('accounts/password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-
-    path('accounts/reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-
-    path('accounts/reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
-    
-     #incluindo as URLs do app surveys
-    path("pesquisa/", include("surveys.urls")),
-
-
-    path('company/', include('companies.urls')),
-    
-    path('projects/', include('projects.urls')),
-
+        name="password_reset_done",
+    ),
+    path(
+        "accounts/reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "accounts/reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
