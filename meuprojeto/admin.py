de86f51815f1a23ee2431e38ab22d382895cec76
João Victor Lopes Dos Santos <133873524+JoaoLopes07@users.noreply.github.com>
2026-01-15 19:53:37 -0300

    fix/refatoração do meuprojeto

---
+++ meuprojeto/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Meu Sistema Administrativo"
+admin.site.site_title = "Painel de Controle"
+admin.site.index_title = "Bem-vindo ao Painel de Controle"
--- meuprojeto/urls.py
+++ meuprojeto/urls.py
@@ -1,55 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from django.views.generic.base import RedirectView
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin_profile'),
+    path('admin/', admin.site.urls),
 
-        #incluindo as URLs do app accounts
-    path(
-        'accounts/',
-        include('accounts.urls')
-        ),
+    path('accounts/', include('accounts.urls')),
 
     path(
         'login/',
-         RedirectView.as_view
-         (url='/accounts/login/',
-        permanent=True)
-        ),
-
-    # URLs de reset de senha padrão do Django
-    path('accounts/password_reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset_form.html'
-         ),
-         name='password_reset'),
-
-    path('accounts/password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='registration/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-
-    path('accounts/reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-
-    path('accounts/reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='registration/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
-    
-     #incluindo as URLs do app surveys
-    path("pesquisa/", include("surveys.urls")),
-
+        RedirectView.as_view(url='/accounts/login/', permanent=True)
+    ),
 
     path('company/', include('companies.urls')),
-    
     path('projects/', include('projects.urls')),
-
+    path('pesquisa/', include('surveys.urls')),
 ]
