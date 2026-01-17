6aefe8485a3ce69293ecc75846c80a106f2927d7
Stefane-7 <225245629+Stefane-7@users.noreply.github.com>
2026-01-17 20:09:31 -0300

    feat(core): criação da logica de redirecionamento

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from core.permissions import (
+    can_manage_companies,
+    can_manage_projects,
+    can_access_projects_area
+)
 
-# Create your views here.
+@login_required
+def redirect_after_login(request):
+    user = request.user
+
+    if can_manage_companies(user):
+        return redirect('dashboard_diretoria')
+
+    if can_manage_projects(user):
+        return redirect('project_list')
+
+    if can_access_projects_area(user):
+        return redirect('project_list')
+
+    return redirect('home')
+
+@login_required
+@user_passes_test(can_manage_companies, login_url="/accounts/home/")
+def dashboard_diretoria(request):
+    return render(request, "dashboard/diretoria/home.html")
+
