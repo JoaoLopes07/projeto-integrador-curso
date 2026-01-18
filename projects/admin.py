6eb148931c86a3379c86bb81266876e1925f4fd1
João Victor Lopes Dos Santos <133873524+JoaoLopes07@users.noreply.github.com>
2026-01-18 19:54:49 -0300

    feat/implementação da gestao de projetos

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-from .models import Project
+from .models import (
+    Project,
+    ProjectLink,
+    ProjectMember,
+    ProjectImage
+)
+
+
+class ProjectLinkInline(admin.TabularInline):
+    model = ProjectLink
+    extra = 1
+
+
+class ProjectMemberInline(admin.TabularInline):
+    model = ProjectMember
+    extra = 1
+
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'company', 'created_at')
     list_filter = ('status', 'company')
     search_fields = ('name',)
+    inlines = [
+        ProjectLinkInline,
+        ProjectMemberInline,
+        ProjectImageInline
+    ]
