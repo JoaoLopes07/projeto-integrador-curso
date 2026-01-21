5620c19ccea34fb320f7cec03e2bf7ac4727400a
Gabrielfs97 <81993925+Gabrielfs97@users.noreply.github.com>
2026-01-21 10:09:33 -0300

    up

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,7 @@
-# build.sh
 #!/usr/bin/env bash
 
 set -o errexit
-
-# Instalar dependências
 pip install -r requirements.txt
-
-# Coletar arquivos estáticos
 python manage.py collectstatic --noinput
-
-# Aplicar migrações
-python manage.py migrate --run-syncdb  # Esta opção pode ajudar
-
-# Ou se preferir forçar
-python manage.py migrate auth
-python manage.py migrate admin
-python manage.py migrate contenttypes
-python manage.py migrate sessions
-python manage.py migrate accounts
+python manage.py makemigrations --noinput  # Add this line
+python manage.py migrate --noinput
